1b49e9915d1e2881deb402a4bf93bc179c5780a8
Sascha Dittmann <Sascha5812@gmx.de>
2023-02-06 10:39:04 +0100

    feat: Add Cloud Composer

---
+++ setup/composer.tf
@@ -0,0 +1,58 @@
+resource "google_storage_bucket" "entsoe_bucket" {
+  name          = var.storage_bucket_name
+  location      = var.data_location
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_composer_environment" "entsoe_composer_env" {
+  name    = "entsoe-composer"
+  project = var.project_id
+  region  = var.project_region
+  depends_on = [
+    google_service_account_iam_member.composer_service_agent_v2,
+    google_project_iam_member.composer_worker
+  ]
+
+  config {
+    software_config {
+      image_version = "composer-2-airflow-2"
+
+      pypi_packages = {
+        "entsoe-py"   = "",
+        "airflow-dbt" = ""
+        #"airflow-dbt-python" = "[bigquery]"
+      }
+    }
+
+    workloads_config {
+      scheduler {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+        count      = 1
+      }
+      web_server {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+      }
+      worker {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+        min_count  = 1
+        max_count  = 3
+      }
+    }
+
+    environment_size = "ENVIRONMENT_SIZE_SMALL"
+
+    node_config {
+      network         = google_compute_network.composer_network.id
+      subnetwork      = google_compute_subnetwork.composer_subnet.id
+      service_account = google_service_account.composer_service_account.name
+    }
+  }
+}

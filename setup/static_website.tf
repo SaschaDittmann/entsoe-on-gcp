f434077bab2f7a545e2039f14d90f1f665764054
Sascha Dittmann <Sascha5812@gmx.de>
2023-02-07 21:12:11 +0100

    feat: remove static website settings

--- setup/static_website.tf
+++ setup/static_website.tf
@@ -4,32 +4,4 @@ resource "google_storage_bucket" "static_website" {
   force_destroy = true
 
   uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
 }
-
-# Upload a simple index.html page to the bucket
-resource "google_storage_bucket_object" "indexpage" {
-  name         = "index.html"
-  content      = "<html><body>Hello World!</body></html>"
-  content_type = "text/html"
-  bucket       = google_storage_bucket.static_website.id
-}
-
-# Upload a simple 404 / error page to the bucket
-resource "google_storage_bucket_object" "errorpage" {
-  name         = "404.html"
-  content      = "<html><body>404!</body></html>"
-  content_type = "text/html"
-  bucket       = google_storage_bucket.static_website.id
-}
-
-# Make bucket public by granting allUsers READER access
-# resource "google_storage_bucket_access_control" "public_rule" {
-#   bucket = google_storage_bucket.static_website.id
-#   role   = "READER"
-#   entity = "allUsers"
-# }

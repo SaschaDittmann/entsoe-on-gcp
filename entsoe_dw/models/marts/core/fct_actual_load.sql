b979728c342b512178d240a065cbcbb6fd37e1da
Sascha Dittmann <Sascha5812@gmx.de>
2023-02-08 17:05:32 +0100

    feat: add total actual load fact table

---
+++ entsoe_dw/models/marts/core/fct_actual_load.sql
@@ -0,0 +1,17 @@
+{{ config (
+    materialized="table",
+    partition_by={
+      "field": "ts",
+      "data_type": "timestamp",
+      "granularity": "day"
+    }
+)}}
+
+with load_forecast as (
+    SELECT *
+    FROM {{ ref('stg_actual_load') }}
+)
+
+SELECT  l.ts AS ts
+        , l.actual_load AS actual_load
+FROM    load_forecast l
